8b3af8999d4d106c44475a85d36150205908e6eb
matthew-wright9630 <matthew.wright9630@gmail.com>
2024-03-04 07:23:05 -0500

    Add index.js, add pictures to initialCards array

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+let goatPicture = {
+  name: goat,
+  link: URL(
+    "https://unsplash.com/photos/a-ram-standing-on-top-of-a-large-rock-G8_JBLFMWTg"
+  ),
+};
+
+let snowyPicture = {
+  name: snowyLandscape,
+  link: URL(
+    "https://unsplash.com/photos/a-black-and-white-photo-of-water-and-rocks-raJ-tYfylhU"
+  ),
+};
+
+let coastPicture = {
+  name: coastLine,
+  link: URL("https://unsplash.com/photos/an-aerial-view-of-a-body-of-water-D1jr0Mevs-c"),
+};
+
+let starPicture = {
+    name: staryBackground,
+    link: URL("https://unsplash.com/photos/two-very-large-objects-in-the-sky-with-stars-ZsJuNhJSiR4"),
+}
+
+let mountainPicutre = {
+    name: mountain,
+    link: URL("https://unsplash.com/photos/a-lone-tree-in-the-middle-of-a-desert-n70vrh_E0Ss"),
+}
+
+let underseaPicture = {
+    name: undersea,
+    link: URL("https://unsplash.com/photos/a-large-group-of-fish-swimming-over-a-coral-reef-bOMVTvE2QFU"),
+}
+
+let initialCards = [goatPicture, snowyPicture, coastPicture, starPicture, mountainPicutre, underseaPicture];
