494081d14f4b1f7705baf74cc1c3dce82c5153f7
matthew-wright9630 <matthew.wright9630@gmail.com>
2024-03-04 07:49:35 -0500

    Add elements to initialCards array

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,38 @@
-let initialCards = [];
-
-let goatPicture = {
-  name: goat,
-  link: URL(
-    "https://unsplash.com/photos/a-ram-standing-on-top-of-a-large-rock-G8_JBLFMWTg"
-  ),
-};
-
-let snowyPicture = {
-  name: snowyLandscape,
-  link: URL(
-    "https://unsplash.com/photos/a-black-and-white-photo-of-water-and-rocks-raJ-tYfylhU"
-  ),
-};
-
-let coastPicture = {
-  name: coastLine,
-  link: URL("https://unsplash.com/photos/an-aerial-view-of-a-body-of-water-D1jr0Mevs-c"),
-};
-
-let starPicture = {
-    name: staryBackground,
-    link: URL("https://unsplash.com/photos/two-very-large-objects-in-the-sky-with-stars-ZsJuNhJSiR4"),
-}
-
-let mountainPicutre = {
-    name: mountain,
-    link: URL("https://unsplash.com/photos/a-lone-tree-in-the-middle-of-a-desert-n70vrh_E0Ss"),
-}
-
-let underseaPicture = {
-    name: undersea,
-    link: URL("https://unsplash.com/photos/a-large-group-of-fish-swimming-over-a-coral-reef-bOMVTvE2QFU"),
-}
-
-initialCards[0] = goatPicture;
-initialCards[1] = snowyPicture;
-initialCards[2] = coastPicture;
-initialCards[3] = starPicture;
-initialCards[4] = mountainPicutre;
-initialCards[5] = underseaPicture;
+const initialCards = [
+  {
+    name: goatPicture,
+    link: URL(
+      "https://unsplash.com/photos/a-ram-standing-on-top-of-a-large-rock-G8_JBLFMWTg"
+    ),
+  },
+  {
+    name: snowyPicture,
+    link: URL(
+      "https://unsplash.com/photos/a-black-and-white-photo-of-water-and-rocks-raJ-tYfylhU"
+    ),
+  },
+  {
+    name: coastLinePicture,
+    link: URL(
+      "https://unsplash.com/photos/an-aerial-view-of-a-body-of-water-D1jr0Mevs-c"
+    ),
+  },
+  {
+    name: staryPicture,
+    link: URL(
+      "https://unsplash.com/photos/two-very-large-objects-in-the-sky-with-stars-ZsJuNhJSiR4"
+    ),
+  },
+  {
+    name: mountainPicture,
+    link: URL(
+      "https://unsplash.com/photos/a-lone-tree-in-the-middle-of-a-desert-n70vrh_E0Ss"
+    ),
+  },
+  {
+    name: underseaPicture,
+    link: URL(
+      "https://unsplash.com/photos/a-large-group-of-fish-swimming-over-a-coral-reef-bOMVTvE2QFU"
+    ),
+  },
+];
