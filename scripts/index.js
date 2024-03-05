76d6597eba4372acf8db4ac2f2bca842af23f233
matthew-wright9630 <matthew.wright9630@gmail.com>
2024-03-05 16:36:59 -0500

    Create the .modal class, which contains the edit profile screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,40 @@
 const initialCards = [
-  {
-    name: "goatPicture",
-    link:
-      "https://unsplash.com/photos/a-ram-standing-on-top-of-a-large-rock-G8_JBLFMWTg"
-    ,
-  },
-  {
-    name: "snowyPicture",
-    link:
-      "https://unsplash.com/photos/a-black-and-white-photo-of-water-and-rocks-raJ-tYfylhU"
-    ,
-  },
-  {
-    name: "coastLinePicture",
-    link: 
-      "https://unsplash.com/photos/an-aerial-view-of-a-body-of-water-D1jr0Mevs-c"
-    ,
-  },
-  {
-    name: "staryPicture",
-    link:
-      "https://unsplash.com/photos/two-very-large-objects-in-the-sky-with-stars-ZsJuNhJSiR4"
-    ,
-  },
-  {
-    name: "mountainPicture",
-    link:
-      "https://unsplash.com/photos/a-lone-tree-in-the-middle-of-a-desert-n70vrh_E0Ss"
-    ,
-  },
-  {
-    name: "underseaPicture",
-    link:
-      "https://unsplash.com/photos/a-large-group-of-fish-swimming-over-a-coral-reef-bOMVTvE2QFU"
-    ,
-  },
-];
-
-console.log(initialCards);
+    {
+      name: "goatPicture",
+      link:
+        "https://unsplash.com/photos/a-ram-standing-on-top-of-a-large-rock-G8_JBLFMWTg"
+      ,
+    },
+    {
+      name: "snowyPicture",
+      link:
+        "https://unsplash.com/photos/a-black-and-white-photo-of-water-and-rocks-raJ-tYfylhU"
+      ,
+    },
+    {
+      name: "coastLinePicture",
+      link: 
+        "https://unsplash.com/photos/an-aerial-view-of-a-body-of-water-D1jr0Mevs-c"
+      ,
+    },
+    {
+      name: "staryPicture",
+      link:
+        "https://unsplash.com/photos/two-very-large-objects-in-the-sky-with-stars-ZsJuNhJSiR4"
+      ,
+    },
+    {
+      name: "mountainPicture",
+      link:
+        "https://unsplash.com/photos/a-lone-tree-in-the-middle-of-a-desert-n70vrh_E0Ss"
+      ,
+    },
+    {
+      name: "underseaPicture",
+      link:
+        "https://unsplash.com/photos/a-large-group-of-fish-swimming-over-a-coral-reef-bOMVTvE2QFU"
+      ,
+    },
+  ];
+  
+  console.log(initialCards);
